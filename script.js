a4e92b06253628e70389f746877970542618a13c
Rahim <114455829+ixlm02@users.noreply.github.com>
2023-12-30 02:26:45 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function showAlert(notice) {
+    if (notice === 'one') {
+        alert("Mi amor, Esha Wahid.");
+    } 
+    if (notice === 'two') {
+        alert("Currently on Home page.");
+    }
+    if (notice === 'three') {
+        alert("Stay tuned!");       
+    }
+    if (notice === 'four') {
+        alert("Currently on About page.");
+    }
+}
